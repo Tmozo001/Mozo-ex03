4e95a5f98142612095de2b911405664f3858083f
Tyler Mozo <Tmozo001@gmail.com>
2021-09-05 19:47:44 -0400

    Initial commit

---
+++ src/main/java/exercise03/solution03.java
@@ -0,0 +1,29 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 first_name last_name
+ */
+/*
+Psuedo
+ask user for a quote using print
+gather the quote with scanner(system.in)(keyboard) and assign to a string
+ask who said the quote with a print
+gather the author with scanner(system.in)(keyboard) and assign to another string
+print out the author and quote along with the desired output
+ */
+package exercise03;
+import java.util.Scanner;
+
+public class solution03 {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("What is the quote?");
+        String quote = input.nextLine();
+
+        System.out.println("Who said it?");
+        String author = input.nextLine();
+
+        System.out.println(author+ " says, \""+quote+"\"");
+    }
+
+}
